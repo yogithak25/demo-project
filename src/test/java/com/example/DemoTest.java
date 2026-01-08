80a9962733380aa7591342aed97a75820a5d9da6
yogithak25 <yogithak25@gmail.com>
2026-01-08 15:59:35 +0530

    Update DemoTest.java

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,12 +1,12 @@
 // src/test/java/com/example/DemoTest.java
-
+package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DemoTest {
 
     @Test
-    public void testFailure() {
-        assertTrue(true);  // This will FAIL
+    public void testSuccess() {
+        assertTrue(true);  // This will pass
     }
 }
