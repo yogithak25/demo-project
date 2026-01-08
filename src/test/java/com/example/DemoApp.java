3839ef7a0ca8a288730defcdb1b500082cff0b99
yogithak25 <yogithak25@gmail.com>
2026-01-08 15:58:02 +0530

    Create DemoApp.java

---
+++ src/test/java/com/example/DemoApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class DemoApp {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println(" Demo App Started Successfully ");
+        System.out.println(" Java Version: " + System.getProperty("java.version"));
+        System.out.println("=================================");
+    }
+}
