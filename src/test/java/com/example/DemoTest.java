620e226895ef70f0f6850a7fe84315d10617ec68
yogithak25 <yogithak25@gmail.com>
2026-01-13 17:25:22 +0530

    Update DemoTest.java

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -3,13 +3,13 @@ package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class DemoTest {
+public class DemoAppTest {
 
     @Test
     public void testStartupMessage() {
         assertEquals(
             "Demo App Started Successfully",
-            DemoApp.STARTUP_MESSAGE
+            DemoApp.getStartupMessage()
         );
     }
 }
